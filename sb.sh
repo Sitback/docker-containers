24da6141a5f7f1f9fe6039fa617ebf701a96cb7c
HudHost <me@me.com>
2015-05-07 21:32:45 +1000

    - skeleton some common docker tasks

--- sb.sh
+++ sb.sh
@@ -1,17 +1,26 @@
 #!/usr/bin/env bash
 
+sb init NAME
+sb NAME run
+sb NAME stop
+sb NAME logs
+
 if ! [ -x "$(type -P boot2docker)" ]; then
   echo "ERROR: script requires boot2docker"
   echo "For Mac try 'brew install boot2docker'"
   exit 1
 fi
 
-run='false'
 helpflag='false'
+run='false'
+stop='false'
+execom=''
 
-while getopts 'rh' flag; do
+while getopts 'rhse:' flag; do
   case "${flag}" in
     r) run="true" ;;
+    s) stop="true" ;;
+    e) execom="" ;;
     h) helpflag='true' ;;
     *) error "Unexpected option ${flag}" ;;
   esac
@@ -24,6 +33,8 @@ if [ $helpflag == 'true' ]; then
   echo "options:"
   echo "-h, --help                show this help"
   echo "-r                        Run project in docker"
+  echo "-s                        Stop the project's docker container"
+  echo "-e [command]              execute a command in the project container"
 
   exit 0
 fi
@@ -37,18 +48,33 @@ if [ $run == 'true' ]; then
 	   -v `pwd`:/var/www/vhosts/mysite.com \
 	   -t docker/web)
 
+  export PROJECT=$ID
+
   echo 'container ID:'
-  echo $ID
+  echo $PROJECT
 
   # Add correct localbox host record for MySQL
   IP=$(ifconfig vboxnet0 | grep inet | awk '{ print $2 }')
 
-  docker exec -d $ID sh -c "echo '$IP localbox' >> /etc/hosts"
+  docker exec -d $PROJECT sh -c "echo '$IP localbox' >> /etc/hosts"
   
-  # docker run -d \
-  #   --add-host=localbox:$(ifconfig vboxnet0 | grep inet | awk '{ print $2 }') \
-  #   -v `pwd`:/var/www/vhosts/mysite.com \
-  #   -t docker/web
+fi
 
-  #docker run -i --rm   --add-host=localbox:$(ifconfig vboxnet0 | grep inet | awk '{ print $2 }')   --net=host   -v /Users/huders2000/Documents/sites/sitback/woolies/dev:/var/www/vhosts/mysite.com   -t docker/web /bin/bash
+# Stop current project
+if [ $stop == 'true' ]; then
+
+  docker kill $PROJECT
+  docker rm $PROJECT
+
+  echo Stopped: $PROJECT
+  
+fi
+
+# Exec in current project
+if [ $execom ]; then
+
+  docker exec -d $PROJECT $execom
+
+  echo "Executed: $ execom in $PROJECT"
+  
 fi
