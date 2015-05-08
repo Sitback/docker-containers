826edc103791dc11d32ad74ead336bc12c026f04
HudHost <me@me.com>
2015-05-08 16:35:02 +1000

    - update sb.sh wrapper script

--- sb.sh
+++ sb.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 
-sb init NAME
-sb NAME run
-sb NAME stop
-sb NAME logs
-
+##
+# Comapability checks
+##
 if ! [ -x "$(type -P boot2docker)" ]; then
   echo "ERROR: script requires boot2docker"
   echo "For Mac try 'brew install boot2docker'"
   exit 1
 fi
 
+##
+# Conatiner to target
+##
+C_NAME=$1
+
+##
+# Options
+##
 helpflag='false'
 run='false'
 stop='false'
@@ -39,42 +45,96 @@ if [ $helpflag == 'true' ]; then
   exit 0
 fi
 
+##
+# Functions
+##
+start() {
+
+  if exists ; then
+
+    echo "ERROR: $C_NAME already exits"
+    echo "       - try 'sb $C_NAME (restart|stop) instead"
+
+  else
+    # Run image and mount current dir as doc root
+     ID=$(docker run -d \
+       --net=host \
+       -v `pwd`:/var/www/vhosts/mysite.com \
+       --name="$C_NAME" \
+       -t docker/web) 
+       
+    if [[ -n $ID ]]; then
+
+      #echo 'container ID:'
+      echo "$C_NAME running in container $ID"
+
+      # Add correct localbox host record for MySQL
+      IP=$(ifconfig vboxnet0 | grep inet | awk '{ print $2 }')
+
+      docker exec -d $ID sh -c "echo '$IP localbox' >> /etc/hosts"
+
+    fi
+  fi
+
+}
+
+stop() {
+
+  if exists ; then
+
+    docker kill $C_NAME
+    docker rm $C_NAME
+
+    echo Stopped: $C_NAME
+
+  fi
+
+}
+
+execcommand() {
+  docker exec -d $C_NAME $execom
+
+  echo "Executed: $ $3 in $C_NAME"
+}
+
+exists() {
+  
+  if ! [[ -n $(docker inspect $C_NAME) ]]; then
+    echo "ERROR: Docker project doesn't exist"
+    return 1
+  fi
+
+  return 0
+}
+
 # Run project in docker
-if [ $run == 'true' ]; then
+#if [ $run == 'true' ]; then
+if [ $2 == 'start' ]; then
 
-  # Run image and mount current dir as doc root
-	 ID=$(docker run -d \
-	   --net=host \
-	   -v `pwd`:/var/www/vhosts/mysite.com \
-	   -t docker/web)
-
-  export PROJECT=$ID
-
-  echo 'container ID:'
-  echo $PROJECT
-
-  # Add correct localbox host record for MySQL
-  IP=$(ifconfig vboxnet0 | grep inet | awk '{ print $2 }')
-
-  docker exec -d $PROJECT sh -c "echo '$IP localbox' >> /etc/hosts"
+  start
   
 fi
 
 # Stop current project
-if [ $stop == 'true' ]; then
+if [ $2 == 'stop' ]; then
 
-  docker kill $PROJECT
-  docker rm $PROJECT
+ stop
+  
+fi
 
-  echo Stopped: $PROJECT
+if [ $2 == 'restart' ]; then
+
+  stop
+  start
   
 fi
 
 # Exec in current project
-if [ $execom ]; then
+if [ $2 == 'exec' ]; then
 
-  docker exec -d $PROJECT $execom
-
-  echo "Executed: $ execom in $PROJECT"
+  execcommand
   
 fi
+
+
+
