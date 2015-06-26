efd48b70f3bae55c13ce9f987e8b4292714c818e
HudHost <paul@drupaul.co.uk>
2015-06-26 11:20:47 +1000

    - add missing packages

--- root/packages.sh
+++ root/packages.sh
@@ -4,9 +4,27 @@ DEBIAN_FRONTEND=noninteractive apt-get -y install supervisor \
 openssh-server \
 apache2 \
 php5 \
-libapache2-mod-php5 \
+php5-common \
+#php-devel \
+#php5-cli \
+php5-gd \
+php5-imap \
+#php-mbstring \
+#php-mcrypt \
+php5-mysql \
+#php-pdo \
+#php-soap \
+#php-xml \
+php5-curl \
+#php-ioncube-loader \
+#libapache2-mod-php5 \
 php5-mysql \
 dnsmasq \
 php-pear \
 php5-dev \
-libcurl3-openssl-dev
+#php-bcmath \
+#libcurl3-openssl-dev \
+#xml-common \
+curl \
+libcurl3 \
+libcurl3-dev
