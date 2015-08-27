db48073c2130b95c5ae5964be74d7ff31f30305e
Chinthaka Godawita <chin@sitback.com.au>
2015-08-27 17:51:01 +1000

    Told mkdir to ignore errors.

--- _scripts/login.sh
+++ _scripts/login.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-mkdir ~/.docker
+mkdir -p ~/.docker
 echo "{\"auths\": {\"https://index.docker.io/v1/\": {\"auth\": \"$DOCKER_HUB_TOKEN\", \"email\": \"chin.godawita@me.com\"} } }" > ~/.docker/config.json
