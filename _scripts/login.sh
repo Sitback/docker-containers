5ab32de919fd0192e2abd75af50faea7a3a15e1d
Chinthaka Godawita <chin.godawita@me.com>
2015-08-30 22:24:05 +1000

    Cleaned up CI build instructions.
    Removed login.sh (not working) and circle.yml (not used).

--- _scripts/login.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p ~/.docker
-echo "{\"auths\": {\"https://index.docker.io/v1/\": {\"auth\": \"$DOCKER_HUB_TOKEN\", \"email\": \"chin.godawita@me.com\"} } }" > ~/.docker/config.json
