c5bfab02d22e747c157ad4ae6fa11c6bd7139d14
Chinthaka Godawita <chin@sitback.com.au>
2015-08-31 17:17:11 +1000

    Attempting Travis publish again.

--- _scripts/publish.rb
+++ _scripts/publish.rb
@@ -10,5 +10,5 @@ login_cmd = "docker login -e=\"#{email}\" -u=\"#{user}\" -p=\"#{pass}\""
 manifest = load_manifest
 load_manifest.each do |path, container|
   image = get_image_name container
-  break unless system! "docker push #{image}"
+  break unless system "#{login_cmd} && docker push #{image}"
 end
