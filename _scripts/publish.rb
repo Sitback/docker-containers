f25f1b68f02783f586ada3122b76ef2a6a40c24a
Chinthaka Godawita <chin@sitback.com.au>
2015-08-31 15:35:37 +1000

    Run docker login on each publish.

--- _scripts/publish.rb
+++ _scripts/publish.rb
@@ -4,10 +4,11 @@ require_relative 'helpers.rb'
 email = ENV['DOCKER_EMAIL']
 user = ENV['DOCKER_USER']
 pass = ENV['DOCKER_PASS']
-system! "docker login -e=\"#{email}\" -u=\"#{user}\" -p=\"#{pass}\""
+
+login_cmd = "docker login -e=\"#{email}\" -u=\"#{user}\" -p=\"#{pass}\""
 
 manifest = load_manifest
 load_manifest.each do |path, container|
   image = get_image_name container
-  break unless system! "docker push #{image}"
+  break unless system! "#{login_cmd} && docker push #{image}"
 end
