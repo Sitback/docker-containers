6899c309d86baf5fd2ca6cccd04e698a298d4776
Chinthaka Godawita <chin.godawita@me.com>
2015-08-31 19:44:56 +1000

    Added more output and fixed potential issue in cache filename.

--- _scripts/travis-build.rb
+++ _scripts/travis-build.rb
@@ -3,14 +3,14 @@ require_relative 'helpers.rb'
 CACHE_DIR = File.expand_path('~/docker').to_s
 
 # Create cache directory if required.
-system "mkdir -p #{CACHE_DIR}"
+system! "mkdir -p #{CACHE_DIR}"
 
 manifest = load_manifest
 load_manifest.each do |name, container|
   image = get_image_name name, container
 
   # Load from cache if we have one.
-  cache_file = File.expand_path(sanitise_filename("#{name}.tar"), CACHE_DIR)
+  cache_file = File.expand_path(sanitise_filename("#{name}.tar"), CACHE_DIR).to_s
   system! "docker load -i #{cache_file}" if File.exist?(cache_file)
 
   system! "docker build -t #{image} #{container['path']}"
