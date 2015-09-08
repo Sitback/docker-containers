764ea38f877118fcedf3315b3b23f4071afe8c1b
Chinthaka Godawita <chin@sitback.com.au>
2015-09-08 12:39:53 +1000

    Fix test docker host on Travis.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,19 +1,22 @@
 require 'serverspec'
 require 'docker'
 
-# Setup Docker URL and certificates.
 set :backend, :docker
-docker_host = ENV['DOCKER_HOST'].dup
-if (ENV['DOCKER_TLS_VERIFY'])
-  cert_path = File.expand_path ENV['DOCKER_CERT_PATH']
-  Docker.options = {
-    client_cert: File.join(cert_path, 'cert.pem'),
-    client_key: File.join(cert_path, 'key.pem')
-  }
-  Excon.defaults[:ssl_ca_file] = File.join(cert_path, 'ca.pem')
-  docker_host.gsub!(/^tcp/,'https')
+
+# Setup Docker URL and certificates if running via a remote Docker Host.
+if ENV['DOCKER_HOST']
+  docker_host = ENV['DOCKER_HOST'].dup
+  if (ENV['DOCKER_TLS_VERIFY'])
+    cert_path = File.expand_path ENV['DOCKER_CERT_PATH']
+    Docker.options = {
+      client_cert: File.join(cert_path, 'cert.pem'),
+      client_key: File.join(cert_path, 'key.pem')
+    }
+    Excon.defaults[:ssl_ca_file] = File.join(cert_path, 'ca.pem')
+    docker_host.gsub!(/^tcp/,'https')
+  end
+  Docker.url = docker_host
 end
-Docker.url = docker_host
 
 # Require all local shared files.
 Dir["#{File.dirname(__FILE__)}/_shared/**/*.rb"].each do |f|
