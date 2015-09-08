68e420e80fb5096820b21c6c7055c60c370523e4
Chinthaka Godawita <chin@sitback.com.au>
2015-09-08 16:06:40 +1000

    More Travis Docker gem debug.

--- _scripts/travis-docker-test.rb
+++ _scripts/travis-docker-test.rb
@@ -1,16 +1,15 @@
 require 'docker'
 
 Docker.url = 'unix:///var/run/docker.sock'
+# Docker.url = ENV['DOCKER_HOST']
 
 puts Docker.info
 
 img_name = 'chinthakagodawita/soe:php5.5'
-image1 = Docker::Image.get(img_name)
+# image1 = Docker::Image.get(img_name)
 image2 = Docker::Image.create('fromImage' => img_name)
-container = Docker::Container.create('Cmd' => ['ls'], 'Image' => image1.id)
+container = Docker::Container.create('Cmd' => ['ls'], 'Image' => image2.id)
 puts container.json
-container2 = Docker::Container.create('Cmd' => ['ls'], 'Image' => image1.id)
-puts container2.json
 puts "Starting container"
 container.start
 puts container.json
