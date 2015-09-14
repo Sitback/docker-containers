c0f45c923859cf46e7219e31524d25c133daa242
Chinthaka Godawita <chin.godawita@me.com>
2015-09-14 20:52:14 +1000

    Moved git-set-mtime to the correct build script this time.

--- _scripts/build.rb
+++ _scripts/build.rb
@@ -1,10 +1,5 @@
 require_relative 'helpers.rb'
 
-# Update mtime to prevent unwanted builds.
-if ENV['CIRCLECI']
-  system! "git set-mtime"
-end
-
 manifest = load_manifest
 load_manifest.each do |name, container|
   image = get_image_name name, container
