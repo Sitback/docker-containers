3b7bcfd798e96fe717f41d671987a59dc69a8ad6
Chinthaka Godawita <chin.godawita@me.com>
2015-09-14 22:36:17 +1000

    Added flag to easily disable caching.

--- _scripts/ci-build.rb
+++ _scripts/ci-build.rb
@@ -2,6 +2,9 @@ require_relative 'helpers.rb'
 
 CACHE_DIR = File.expand_path('~/docker').to_s
 
+# Toggle to disable caching.
+CACHE_ENABLED = true;
+
 # Create cache directory if required.
 system! "mkdir -p #{CACHE_DIR}"
 
@@ -11,7 +14,7 @@ load_manifest.each do |name, container|
 
   # Load from cache if we have one.
   cache_file = File.expand_path(sanitise_filename("#{name}.tar"), CACHE_DIR).to_s
-  if File.exist?(cache_file)
+  if File.exist?(cache_file) and CACHE_ENABLED
     system! "docker load -i #{cache_file}", true
   else
     # Pull existing from Docker Hub, ignoring errors.
@@ -21,5 +24,5 @@ load_manifest.each do |name, container|
   system! "docker build -t #{image} #{container['path']}"
 
   # Cache image for next build.
-  system! "docker save #{image} > #{cache_file}", true
+  system! "docker save #{image} > #{cache_file}", true if CACHE_ENABLED
 end
