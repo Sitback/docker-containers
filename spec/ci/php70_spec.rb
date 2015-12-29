b2e3ac0fc75521dd00c87cbc4e56c178d0237be4
Jonathan Rhodes <gig745@gmail.com>
2015-12-29 22:40:08 +1100

    Set php_packages and apache_php_mod for CI php 7.0 spec.

--- spec/ci/php70_spec.rb
+++ spec/ci/php70_spec.rb
@@ -3,6 +3,23 @@ require 'spec_helper'
 describe 'PHP 7.0 CI' do
   include_context 'ci' do
     let(:php_version) { '7.0' }
+
+    let(:php_packages) { [
+     'apache2',
+     'php7.0',
+     'php7.0-cli',
+     'mysql-client',
+     'memcached',
+     'php7.0-gd',
+     'php7.0-dev',
+     'php7.0-curl',
+     'php7.0-mysql',
+     'php-memcached',
+     'php-soap',
+     'php-pear'
+    ] }
+
+    let(:apache_php_mod) { 'php7_module' }
   end
 
   before(:all) do
