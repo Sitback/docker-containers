c5e0f8ca660b78e8a8c907f355b4f4d1160c9a8e
Chin Godawita <chin@sitback.com.au>
2016-10-25 10:56:36 +1100

    Fixed Apache version check for SOE packages.

--- spec/soe/php70_spec.rb
+++ spec/soe/php70_spec.rb
@@ -7,6 +7,7 @@ describe 'PHP 7.0 SOE' do
     let(:php_packages) { Constants::PHP70_PACKAGES }
     let(:apache_php_mod) { 'php7_module' }
     let(:ubuntu_version) { '16.04' }
+    let(:apache_version) { '2.4.18' }
   end
 
   before(:all) do
