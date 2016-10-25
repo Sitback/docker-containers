93bfcbae46258d18802a7dc3e20147bf40174c01
Chin Godawita <chin@sitback.com.au>
2016-10-26 10:21:10 +1100

    Added retry support to publish command.

--- _scripts/publish.rb
+++ _scripts/publish.rb
@@ -4,11 +4,15 @@ require_relative 'helpers.rb'
 email = ENV['DOCKER_EMAIL']
 user = ENV['DOCKER_USER']
 pass = ENV['DOCKER_PASS']
+retries = 3
 
 system "docker login -e=\"#{email}\" -u=\"#{user}\" -p=\"#{pass}\""
 
 manifest = load_manifest
 load_manifest.each do |name, container|
   image = get_image_name name, container
-  system! "docker push #{image}"
+  retries.times do
+    system "docker push #{image}"
+    break if $?.success?
+  end
 end
