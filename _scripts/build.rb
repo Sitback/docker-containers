ecdf471697e014601abf40f31820b32872d357ba
Chin Godawita <chin@sitback.com.au>
2017-07-11 14:44:22 +1000

    Added caching to correct build script.

--- _scripts/build.rb
+++ _scripts/build.rb
@@ -3,5 +3,5 @@ require_relative 'helpers.rb'
 manifest = load_manifest
 load_manifest.each do |name, container|
   image = get_image_name name, container
-  system! "docker build --cache-from #{image} -t #{image} #{container['path']}"
+  system! "docker build -t #{image} #{container['path']}"
 end
--- _scripts/ci-build.rb
+++ _scripts/ci-build.rb
@@ -13,15 +13,15 @@ load_manifest.each do |name, container|
   image = get_image_name name, container
 
   # Load from cache if we have one.
-  cache_file = File.expand_path(sanitise_filename("#{name}.tar"), CACHE_DIR).to_s
-  if File.exist?(cache_file) and CACHE_ENABLED
-    system! "docker load -i #{cache_file}", true
-  else
-    # Pull existing from Docker Hub, ignoring errors.
-    system! "docker pull #{image}", true
-  end
+  # cache_file = File.expand_path(sanitise_filename("#{name}.tar"), CACHE_DIR).to_s
+  # if File.exist?(cache_file) and CACHE_ENABLED
+  #   system! "docker load -i #{cache_file}", true
+  # else
+  #   # Pull existing from Docker Hub, ignoring errors.
+  #   system! "docker pull #{image}", true
+  # end
 
-  system! "docker build -t #{image} #{container['path']}"
+  system! "docker build --cache-from #{image} -t #{image} #{container['path']}"
 
   # Cache image for next build.
   system! "docker save #{image} > #{cache_file}", true if CACHE_ENABLED
