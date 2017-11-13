c5eda690bc20fd62fb0031fd422df0c91f32cda4
Chin Godawita <chin.godawita@me.com>
2017-11-13 09:49:15 +0000

    Reverted caching logic to pre-Codeship.

--- _scripts/ci-build.rb
+++ _scripts/ci-build.rb
@@ -12,17 +12,16 @@ manifest = load_manifest
 load_manifest.each do |name, container|
   image = get_image_name name, container
 
-  # Load from cache if we have one.
-  # cache_file = File.expand_path(sanitise_filename("#{name}.tar"), CACHE_DIR).to_s
-  # if File.exist?(cache_file) and CACHE_ENABLED
-  #   system! "docker load -i #{cache_file}", true
-  # else
-  #   # Pull existing from Docker Hub, ignoring errors.
-  #   system! "docker pull #{image}", true
-  # end
+  Load from cache if we have one.
+  cache_file = File.expand_path(sanitise_filename("#{name}.tar"), CACHE_DIR).to_s
+  if File.exist?(cache_file) and CACHE_ENABLED
+    system! "docker load -i #{cache_file}", true
+  else
+    # Pull existing from Docker Hub, ignoring errors.
+    system! "docker pull #{image}", true
+  end
 
-  # system! "docker pull #{image}"
-  system! "docker build --cache-from #{image} -t #{image} #{container['path']}"
+  system! "docker build -t #{image} #{container['path']}"
 
   # Cache image for next build.
   system! "docker save #{image} > #{cache_file}", true if CACHE_ENABLED
