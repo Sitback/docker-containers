c7df0628c0b7690fc9d197270f37c6e2df5cc69a
Chin Godawita <chin.godawita@me.com>
2017-12-04 15:01:28 +0000

    Added tests for PHP 7.2 container (#8).

---
+++ spec/php/72_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe 'PHP 7.2' do
+  include_context 'php' do
+    let(:php_version) { '7.2' }
+    let(:php_packages) { Constants::PHP71_PACKAGES }
+    let(:apache_version) { '2.4.18' }
+    let(:apache_php_mod) { 'php7_module' }
+    let(:ubuntu_version) { '16.04' }
+  end
+
+  before(:all) do
+    image_name = "#{Constants::IMAGE_PREFIX}php:7.2"
+    set :os, family: Constants::OS_FAMILY
+    set :docker_image, get_docker_image_id(image_name)
+  end
+end
---
+++ spec/soe/php72_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe 'PHP 7.2 SOE' do
+  include_context 'soe' do
+    let(:php_version) { '7.2' }
+    let(:soe_packages) { Constants::PHP70_SOE_PACKAGES }
+    let(:php_packages) { Constants::PHP71_PACKAGES }
+    let(:apache_php_mod) { 'php7_module' }
+    let(:ubuntu_version) { '16.04' }
+    let(:apache_version) { '2.4.18' }
+  end
+
+  before(:all) do
+    image_name = "#{Constants::IMAGE_PREFIX}soe:php7.2"
+    set :os, family: Constants::OS_FAMILY
+    set :docker_image, get_docker_image_id(image_name)
+  end
+
+  describe 'SOE PHP Install' do
+    describe command('php -m') do
+      its(:stdout) { should include('xdebug') }
+    end
+  end
+end
